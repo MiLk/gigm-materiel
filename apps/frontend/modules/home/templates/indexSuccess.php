b9c9d6f860002f051ad4ae7fd9225620b167d13b
Emilien Kenler <emilien.kenler@etu.utc.fr>
2011-03-21 14:22:29 +0100

    Design frontend/home
    Signed-off-by: Emilien Kenler <emilien.kenler@etu.utc.fr>

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -1,20 +1,22 @@
-<div class="haut">
-  Haut
-</div>
-<div class="content">
-  <ul>
-    <?php foreach($disponible as $row): ?>
-      <li><?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?></li>
-    <?php endforeach ?>
-  </ul>
-  <ul>
-    <?php foreach($emprunte as $row): ?>
-      <li><?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?></li>
-    <?php endforeach ?>
-  </ul>
-  <ul>
-    <?php foreach($assemble as $row): ?>
-      <li><?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?></li>
-    <?php endforeach ?>
-  </ul>
+<div id="home">
+  <div class="haut">
+    Haut
+  </div>
+  <div class="content">
+    <ul class="disponible">
+      <?php foreach($disponible as $row): ?>
+        <li><?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?></li>
+      <?php endforeach ?>
+      </ul>
+      <ul class="emprunt">
+      <?php foreach($emprunte as $row): ?>
+          <li><?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?></li>
+      <?php endforeach ?>
+        </ul>
+        <ul class="assemblage">
+      <?php foreach($assemble as $row): ?>
+            <li><?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?></li>
+      <?php endforeach ?>
+    </ul>
+  </div>
 </div>
