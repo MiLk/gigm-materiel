5b6b0d8ffb6c0092f286cf39a51ba337da0d6643
Guillaume George <guillaume.george@etu.utc.fr>
2011-03-21 15:36:18 +0100

    Formulaire d'emprunt sur Home
    Signed-off-by: Guillaume George <guillaume.george@etu.utc.fr>

--- apps/frontend/modules/emprunt/templates/emprunterSuccess.php
+++ apps/frontend/modules/emprunt/templates/emprunterSuccess.php
@@ -1,5 +1,7 @@
 <form method="post" action="<?php echo url_for('emprunt/emprunter') ?>">
     <?php
+        echo $form->renderHiddenFields();
         echo $form;
     ?>
+    <input type="submit" value="Emprunter"/>
 </form>
