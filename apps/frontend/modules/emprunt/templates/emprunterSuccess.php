e6ff5a6601ffd874d9810433e8bc459fbfdd65dc
Guillaume George <guillaume.george@etu.utc.fr>
2011-03-21 15:40:52 +0100

    Noob
    Signed-off-by: Guillaume George <guillaume.george@etu.utc.fr>

--- apps/frontend/modules/emprunt/templates/emprunterSuccess.php
+++ apps/frontend/modules/emprunt/templates/emprunterSuccess.php
@@ -1,6 +1,5 @@
 <form method="post" action="<?php echo url_for('emprunt/emprunter') ?>">
     <?php
-        echo $form->renderHiddenFields();
         echo $form;
     ?>
     <input type="submit" value="Emprunter"/>
