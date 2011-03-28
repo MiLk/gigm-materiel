77c1d1e5cdfb88584bf9c9f7e76f2e406f8a9b14
Guillaume George <guillaume.george@etu.utc.fr>
2011-03-28 14:44:35 +0200

    Correction liste des emprunts par matriel
    Signed-off-by: Guillaume George <guillaume.george@etu.utc.fr>

--- apps/frontend/modules/emprunt/templates/showSuccess.php
+++ apps/frontend/modules/emprunt/templates/showSuccess.php
@@ -1,6 +1,6 @@
 <?php use_helper('Date') ?>
 <ul>
   <?php foreach($emprunts as $row): ?>
-    <li><?php echo $row->getUser() ?> a emprunté <?php echo $row->getNombre() ?>x <?php $row->getMateriel() ?> le <?php ucfirst(format_date($row->getCreatedAt(),'P','fr')) ?></li>
+    <li><?php echo $row->getUser() ?> a emprunté <?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?> le <?php echo ucfirst(format_date($row->getCreatedAt(),'P','fr')) ?></li>
   <?php endforeach ?>
 </ul>
