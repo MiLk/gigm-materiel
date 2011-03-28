3836da8c5d9e51d45fcae276817f06bcf964a597
Guillaume George <guillaume.george@etu.utc.fr>
2011-03-28 11:02:27 +0200

    Ajout de notice/error
    Signed-off-by: Guillaume George <guillaume.george@etu.utc.fr>

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -1,4 +1,12 @@
 <div id="home">
+
+    <?php if ($sf_user->hasFlash('notice')): ?>
+        <div class="flash_notice"><?php echo $sf_user->getFlash('notice') ?></div>
+    <?php endif ?>
+
+    <?php if ($sf_user->hasFlash('error')): ?>
+        <div class="flash_error"><?php echo $sf_user->getFlash('error') ?></div>
+    <?php endif ?>
     <div class="haut">
         Mes emprunts :
         <ul>
@@ -6,19 +14,19 @@
                 <li><?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?> - <a href="<?php echo url_for('emprunt/rendre?emprunt='.$row->getPrimaryKey()) ?>">Rendre</a></li>
             <?php endforeach ?>
         </ul>
-        </div>
-        <div class="content">
-            <ul class="disponible">
+     </div>
+     <div class="content">
+        <ul class="disponible">
             <?php foreach($disponible as $row): ?>
                 <li><?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?> - <a href="<?php echo url_for('emprunt/emprunter?materiel='.$row->getMaterielId()) ?>">Emprunter</a></li>
             <?php endforeach ?>
-                </ul>
-                <ul class="emprunt">
+        </ul>
+        <ul class="emprunt">
             <?php foreach($emprunte as $row): ?>
                         <li><?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?></li>
             <?php endforeach ?>
-                    </ul>
-                    <ul class="assemblage">
+        </ul>
+        <ul class="assemblage">
             <?php foreach($assemble as $row): ?>
                             <li><?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?></li>
             <?php endforeach ?>
