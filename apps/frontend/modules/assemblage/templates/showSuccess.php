e23bb6afcc31fdce5eff6c62a0bab6b9a6e291a6
Guillaume George <guillaume.george@etu.utc.fr>
2011-04-11 11:21:35 +0200

    assemblages corrections
    Signed-off-by: Guillaume George <guillaume.george@etu.utc.fr>

--- apps/frontend/modules/assemblage/templates/showSuccess.php
+++ apps/frontend/modules/assemblage/templates/showSuccess.php
@@ -1,7 +1,7 @@
 <?php use_helper('Date') ?>
   <ul>
     <?php foreach($assemblage as $row): ?>
-      <li><?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?> assemblés sur <?php echo $row->getEquipement() ?> le <?php echo ucfirst(format_date($row->getCreatedAt(),'P','fr'))?></li>
+      <li><?php echo $row->getNombre() ?>x <?php echo ucfirst($row->getMateriel()) ?> assemblés sur <strong><?php echo ucfirst($row->getEquipement()) ?></strong> le <?php echo ucfirst(format_date($row->getCreatedAt(),'P','fr'))?></li>
     <?php endforeach ?>
   </ul>
 <a href="<?php echo url_for('homepage') ?>">Retour</a>
