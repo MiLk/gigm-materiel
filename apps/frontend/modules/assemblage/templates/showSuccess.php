32ad29df1d9620737c7362fd68b77be94b7de0d8
Emilien Kenler <emilien.kenler@etu.utc.fr>
2011-04-11 11:41:30 +0200

    Modification esthetique
    Signed-off-by: Emilien Kenler <emilien.kenler@etu.utc.fr>

--- apps/frontend/modules/assemblage/templates/showSuccess.php
+++ apps/frontend/modules/assemblage/templates/showSuccess.php
@@ -1,7 +1,7 @@
 <?php use_helper('Date') ?>
   <ul>
     <?php foreach($assemblage as $row): ?>
-      <li><?php echo $row->getNombre() ?>x <?php echo ucfirst($row->getMateriel()) ?> assemblés sur <strong><?php echo ucfirst($row->getEquipement()) ?></strong> le <?php echo ucfirst(format_date($row->getCreatedAt(),'P','fr'))?></li>
+      <li><?php echo $row->getNombre() ?>x <strong><?php echo $row->getMateriel() ?></strong> assemblés sur <strong><?php echo ucfirst($row->getEquipement()) ?></strong> le <strong><?php echo ucfirst(format_date($row->getCreatedAt(),'P','fr'))?></strong></li>
     <?php endforeach ?>
   </ul>
 <a href="<?php echo url_for('homepage') ?>">Retour</a>
--- apps/frontend/modules/emprunt/templates/showSuccess.php
+++ apps/frontend/modules/emprunt/templates/showSuccess.php
@@ -1,7 +1,7 @@
 <?php use_helper('Date') ?>
 <ul>
     <?php foreach($emprunts as $row): ?>
-      <li><?php echo $row->getUser() ?> a emprunté <?php echo $row->getNombre() ?>x <?php echo $row->getMateriel() ?> le <?php echo ucfirst(format_date($row->getCreatedAt(),'P','fr')) ?></li>
+      <li><strong><?php echo $row->getUser() ?></strong> a emprunté <?php echo $row->getNombre() ?>x <strong><?php echo $row->getMateriel() ?></strong> pour le groupe <strong><?php echo $row->getGroup() ?></strong> le <strong><?php echo ucfirst(format_date($row->getCreatedAt(),'P','fr')) ?></strong></li>
     <?php endforeach ?>
 </ul>
 <a href="<?php echo url_for('homepage') ?>">Retour</a>
