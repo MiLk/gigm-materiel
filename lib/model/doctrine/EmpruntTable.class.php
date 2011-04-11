19e79a6e8b1815856b9a8ad15183075fb0dd2d5e
Guillaume George <guillaume.george@etu.utc.fr>
2011-04-11 14:38:28 +0200

    fonction assembler - corrections
    Signed-off-by: Guillaume George <guillaume.george@etu.utc.fr>

--- lib/model/doctrine/EmpruntTable.class.php
+++ lib/model/doctrine/EmpruntTable.class.php
@@ -55,7 +55,7 @@ class EmpruntTable extends Doctrine_Table
   public function getOneById($emprunt_id)
   {
     $q = $this->createQuery('q')
-        ->where('q.emprunt_id ='.$emprunt_id);
+        ->where('q.id ='.$emprunt_id);
 
     return $q;
   }
